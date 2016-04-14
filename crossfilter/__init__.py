437c3a614e42d6978cedbf4614fe3c066989c5ba
Jonas Heylen <jonas.heylen@gmail.com>
2016-04-14 22:30:34 +0200

    two barcharts

--- crossfilter/__init__.py
+++ crossfilter/__init__.py
@@ -1,12 +1,13 @@
-from IPython.display import Javascript
+from IPython.display import HTML, Javascript, display
 
 
-def load_js():
-    return Javascript("""require.config({
+def load_resources():
+    display(Javascript("""require.config({
     paths: {
         'd3': '//cdnjs.cloudflare.com/ajax/libs/d3/3.5.16/d3.min',
         'crossfilter': '//cdnjs.cloudflare.com/ajax/libs/crossfilter/1.3.12/crossfilter.min',
         'dc': '//cdnjs.cloudflare.com/ajax/libs/dc/1.7.5/dc.min',
+        'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
     },
     shim: {
         'crossfilter': {
@@ -14,7 +15,7 @@ def load_js():
             exports: 'crossfilter'
         }
     }
-});""")
+});"""), HTML('<link href="https://cdnjs.cloudflare.com/ajax/libs/dc/1.7.5/dc.min.css" rel="stylesheet" type="text/css">'))
 
 
 def dataframe_as_js(df, name='crossfilterData'):
